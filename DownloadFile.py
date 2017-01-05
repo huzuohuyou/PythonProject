1a2fbbc6ff10379ee26ac2d8b317635323b488bc
Hailong Wu <wuhl@bimt.com>
2017-01-05 14:05:25 +0800

    3.0.0.1
    1.添加数据持久化功能，记录最后下载文件的id,及发生错误的id;
    2.运行脚本时先跑一遍未下载成功的id;然后在接着最后的id下载全文；

--- DownloadFile.py
+++ DownloadFile.py
@@ -22,15 +22,61 @@ def callbackfunc(blocknum, blocksize, totalsize):
     if percent == 100:
         print('')
         #input('输入任意键继续...')
-def downimg():
-#3128072
-    for id in range(3128072,9999999):
+
+class DLog:
+    def __init__(self, lastId, exArray):
+      self.lastId = lastId
+      self.exArray = exArray
+
+    def logExId(self,lastId):
+        if(lastId in self.exArray):
+            pass
+        else:
+            self.exArray.append(lastId)
+            output = open('dlog.pkl', 'wb')
+            # Pickle dictionary using protocol 0.
+            pickle.dump(self, output,-1)
+            output.close()
+            print(self.exArray)
+
+    def logLastId(self,lastId):
+        self.lastId=lastId
+        output = open('dlog.pkl', 'wb')
+        # Pickle dictionary using protocol 0.
+        pickle.dump(self, output,-1)
+        output.close()
+
+    def getLastId(self):
+        return self.lastId
+
+def downingArray(dlog,array):
+    for id in array:
+        print("reloading exid " +str(dlog.exArray)+'...')
         try:
             url='http://pubmedcentralcanada.ca/pmcc/articles/PMC{id}/pdf/{id}.pdf'.format(id=id)
             print("PMCID:"+str(id)+url)
-            filename=os.path.basename(url)
-            urllib.request.urlretrieve(url, "E://Docs//"+filename, callbackfunc)
+            filename='PMC{ID}'.format(id=os.path.basename(url))
+            urllib.request.urlretrieve(url, filename, callbackfunc)
+            dlog.logLastId(id)
         except:
-            pass
+            dlog.logExId(id)
+
+def downimg():
+#3128072
+    dlog=None
+    if(os.path.exists("dlog.pkl")):
+
+        pkl_file = open('dlog.pkl', 'rb')
+        dlog = pickle.load(pkl_file)
+        pkl_file.close()
+        print("exist file dlog.pkl load dlog lastId:{lastId}...".format(lastId=dlog.lastId))
+    else:
+        dlog = DLog(3128072,[])
+        print("didn't exist file dlog.pkl create new one ...")
+    #失败数据重载
+    downingArray(dlog,dlog.exArray)
+    #新数据下载
+    downingArray(dlog,range(dlog.lastId,99999999))
+
 #启动线程下载
 threading.Thread(target=downimg,args=('')).start()
